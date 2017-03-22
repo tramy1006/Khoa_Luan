7944d1f9b5553b9f8987fe94f9c0f61632b8c358
Đoàn My <tramy100695@gmail.com>
2017-03-22 18:57:17 +0700

    fix 6

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -66,7 +66,10 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+
             'password' => bcrypt($data['password']),
+            
+
         ]);
     }
 }
