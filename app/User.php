1e2a775aed699040ec182f8d879a84c39572bd4a
Đoàn My <tramy100695@gmail.com>
2017-03-22 18:36:06 +0700

    fix 6

--- app/User.php
+++ app/User.php
@@ -11,8 +11,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+
     protected $fillable = [
-        'name', 'email', 'password', 'role',
+        'name', 'email', 'password',
     ];
 
     /**
