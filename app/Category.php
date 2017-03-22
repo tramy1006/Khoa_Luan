9a231c4243f9b429b04dbe19f71f6b563ebfc3d6
Đoàn My <tramy100695@gmail.com>
2017-03-22 16:36:22 +0700

    fix 2

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = "categories";
+    protected $fillable = ['name'];
+    
     public function lessons()
     {
     	return $this->hasMany('App\Lesson','cate_id', 'id');
